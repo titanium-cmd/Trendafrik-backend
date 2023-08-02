835ed802e23130ad44b77c5e01f00ed825805db6
titanium-cmd <nkansahisaac41@gmail.com>
2023-08-02 20:35:51 +0000

    bug fixes

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Create an instance of the Express application
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 // Apply middleware for handling CORS, logging requests, parsing JSON and URL-encoded data, and handling cookies
 app.use(cors()); // Allow Cross-Origin Resource Sharing (CORS)
@@ -16,5 +18,11 @@ app.use(express.urlencoded({ extended: false })); // Parse incoming URL-encoded 
 app.use('/api/v1/users', require('./routes/userRoute')); // User-related routes
 app.use('/api/v1/quizzes', require('./routes/quizRoute')); // Quiz-related routes
 
-// Export the Express application to be used in other files
-module.exports = app;
+mongoose.connect(process.env.MONGO_DB_URI).then(() => {
+  console.log('MONGODB CONNECTED');
+  app.listen(PORT, () => {
+    console.log(`SERVER RUNNING ON PORT ${PORT}`);
+  })
+}).catch((error) => {
+  console.log('MONGODB CONNECTION ERROR:: ', error.message);
+});
